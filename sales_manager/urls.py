28b51f3d7c1b125f73fcdde5cb47a943bf0d7bc5
VovoSL <SlizhVV@yandex.ru>
2021-08-12 01:38:20 +0300

    changed url book_detail

--- sales_manager/urls.py
+++ sales_manager/urls.py
@@ -3,6 +3,6 @@ from sales_manager import views
 
 
 urlpatterns = [
-    path('book_detail-<int:book_id>/', views.book_detail, name="book-detail"),
+    path('book_detail/<int:book_id>/', views.book_detail, name="book-detail"),
     path('', views.main_page),
 ]
