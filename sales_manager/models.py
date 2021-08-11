53407c07ad193544dde7bf3add1af5e20b05e07f
VovoSL <SlizhVV@yandex.ru>
2021-08-12 01:28:00 +0300

    view for all books and book_detail page

--- sales_manager/models.py
+++ sales_manager/models.py
@@ -3,7 +3,23 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=124, db_index=True)
-    text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_publish = models.DateField(auto_now_add=True, db_index=True)
+    title = models.CharField(
+        max_length=124,
+        db_index=True,
+        verbose_name="Название"
+    )
+    text = models.TextField(verbose_name="Описание")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор"
+    )
+    date_publish = models.DateField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name="Дата публикации"
+    )
+
+    # Возвращает название в админке, вместо book1 , 2 и т.д.
+    def __str__(self):
+        return self.title
